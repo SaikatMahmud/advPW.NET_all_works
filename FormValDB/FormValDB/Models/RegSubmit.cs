af99f66c94070c7099c270f251e17c45b8684a9e
Saikat Mahmud <saikat8486@gmail.com>
2023-02-15 03:02:09 +0600

    update repo

---
+++ FormValDB/FormValDB/Models/RegSubmit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormValDB.Models
+{
+    public class RegSubmit
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        [Required]
+        public string[] Hobby { get; set; }
+        [Required]
+        public DateTime Dob { get; set; }
+    }
+}
