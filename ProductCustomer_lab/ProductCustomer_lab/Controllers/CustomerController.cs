4bd9bfaf97bdccd8c438c2e4c6b2511c82700919
Saikat Mahmud <saikat8486@gmail.com>
2023-02-16 08:12:26 +0600

    ecom_test lab

---
+++ ProductCustomer_lab/ProductCustomer_lab/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using ProductCustomer_lab.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductCustomer_lab.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer cus)
+        {
+            var db = new ECom_asp_testEntities();
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(cus);
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+            return View(cus);
+        }
+
+        public ActionResult List()
+        {
+            var db = new ECom_asp_testEntities();
+            var cus = db.Customers.ToList();
+            return View(cus);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ECom_asp_testEntities();
+            var cus = (from c in db.Customers
+                       where c.Id == id //id has spelling mistake in DB
+                       select c).SingleOrDefault();
+            return View(cus);
+        }
+        [HttpPost]
+        public ActionResult Edit(Customer updated)
+        {
+            var db = new ECom_asp_testEntities();
+            var extCus = (from c in db.Customers
+                          where c.Id == updated.Id
+                          select c).SingleOrDefault();
+            db.Entry(extCus).CurrentValues.SetValues(updated);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public ActionResult Delete(int id)
+        {
+            var db = new ECom_asp_testEntities();
+            var delCus = (from c in db.Customers
+                          where c.Id == id //id has spelling mistake in DB
+                          select c).SingleOrDefault();
+            db.Customers.Remove(delCus);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ ProductCustomer_lab/ProductCustomer_lab/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using ProductCustomer_lab.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductCustomer_lab.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult Create()
+        {   
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product pd)
+        {
+            var db =new ECom_asp_testEntities();
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(pd);
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+            return View(pd);
+        }
+        public ActionResult List()
+        {
+            var db = new ECom_asp_testEntities();
+            var pd = db.Products.ToList();
+            return View(pd);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ECom_asp_testEntities();
+            var product = (from p in db.Products
+                           where p.ld == id //id has spelling mistake in DB
+                           select p).SingleOrDefault();
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product updated)
+        {
+            var db = new ECom_asp_testEntities();
+            var extProduct = (from p in db.Products
+                              where p.ld == updated.ld
+                              select p).SingleOrDefault();
+            db.Entry(extProduct).CurrentValues.SetValues(updated);
+            db.SaveChanges();
+            return RedirectToAction("List");
+            //db.Students.Remove(extstudent);
+        }
+        public ActionResult Delete(int id)
+        {
+            var db = new ECom_asp_testEntities();
+            var delProduct = (from p in db.Products
+                              where p.ld == id //id has spelling mistake in DB
+                              select p).SingleOrDefault();
+            db.Products.Remove(delProduct);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
